b23aafe70e90e65d4a7e310b9da548d7acc8418b
Awhiteweb <alex_p_white@yahoo.co.uk>
2024-02-12 20:10:00 +0000

    Update StorageFactory.js

--- Projects/Open-Storage/SA/Utilities/StorageFactory.js
+++ Projects/Open-Storage/SA/Utilities/StorageFactory.js
@@ -3,3 +3,17 @@ exports.newOpenStorageUtilitiesStorageFactory = {
     }
 }
 
+function createStorageClient(containerType) {
+    switch (containerType) {
+        case 'Github Storage Container': {
+            return SA.projects.openStorage.utilities.githubStorage
+        }
+        case 'AWS S3 Storage Container': {
+            return SA.projects.openStorage.utilities.awsS3Storage
+        }
+        case 'Superalgos Storage Container': {
+            // TODO Build the Superalgos Storage Provider
+            break
+        }
+    }
+}
